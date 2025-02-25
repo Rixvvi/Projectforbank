155e1b05bf9ec6f65232f11bec3dc5e9b03ec9b0
Elizabeth <rxxivkey@gmail.com>
2025-02-25 15:41:56 +0300

    masks after testing

--- src/masks.py
+++ src/masks.py
@@ -2,21 +2,27 @@ def get_mask_card_number(card_number: str) -> str:
     """Функция принимает на вход номер карты и возвращает её маску"""
     masked_card = list()
 
-    for number in card_number[:6]:
-        masked_card.append(str(number))
+    if card_number.isdigit():
 
-    for number in card_number[6:-4]:
-        masked_card.append("*")
+        if len(card_number) == 16:
+            for number in card_number[:6]:
+                masked_card.append(str(number))
 
-    for number in card_number[-4:]:
-        masked_card.append(str(number))
+            for number in card_number[6:-4]:
+                masked_card.append("*")
 
-    mask_card = "".join(masked_card)
+            for number in card_number[-4:]:
+                masked_card.append(str(number))
 
-    return " ".join(mask_card[i: i + 4] for i in range(0, len(mask_card), 4))
+            mask_card = "".join(masked_card)
 
+            return " ".join(mask_card[i: i + 4] for i in range(0, len(mask_card), 4))
 
-get_mask_card_number(input())
+        else:
+            return "Введите 16-ти значный номер карты"
+
+    else:
+        return "Введите номер карты"
 
 
 def get_mask_account(account: str) -> str:
@@ -29,6 +35,3 @@ def get_mask_account(account: str) -> str:
         mask_account.append(str(number))
 
     return "".join(mask_account)
-
-
-get_mask_account(input())
