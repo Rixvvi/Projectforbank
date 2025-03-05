a442d1fb43039baf2a32d231b7cf9b496e010b9c
Elizabeth <rxxivkey@gmail.com>
2025-03-05 15:55:45 +0300

    writed tests for generators

---
+++ tests/test_generators.py
@@ -0,0 +1,56 @@
+import pytest
+
+from generators import filter_by_currency, transaction_descriptions, card_number_generator
+
+
+def test_filter_by_currency_usd(transactions):
+    usd_transactions = filter_by_currency(transactions, "USD")
+    assert next(usd_transactions) == {
+            "id": 939719570,
+            "state": "EXECUTED",
+            "date": "2018-06-30T02:08:58.425572",
+            "operationAmount": {
+                "amount": "9824.07",
+                "currency": {
+                    "name": "USD",
+                    "code": "USD"
+                }
+            },
+            "description": "Перевод организации",
+            "from": "Счет 75106830613657916952",
+            "to": "Счет 11776614605963066702"
+        }
+
+
+def test_filter_by_currency_rub(transactions):
+    usd_transactions = filter_by_currency(transactions, "RUB")
+    assert next(usd_transactions) == {
+            "id": 873106923,
+            "state": "EXECUTED",
+            "date": "2019-03-23T01:09:46.296404",
+            "operationAmount": {
+                "amount": "43318.34",
+                "currency": {
+                    "name": "руб.",
+                    "code": "RUB"
+                }
+            },
+            "description": "Перевод со счета на счет",
+            "from": "Счет 44812258784861134719",
+            "to": "Счет 74489636417521191160"
+        }
+
+
+def test_transaction_descriptions(transactions):
+    descriptions = transaction_descriptions(transactions)
+    assert next(descriptions) == 'Перевод организации'
+    assert next(descriptions) == 'Перевод со счета на счет'
+    assert next(descriptions) == 'Перевод со счета на счет'
+    assert next(descriptions) == 'Перевод с карты на карту'
+
+
+def test_card_number_generator():
+    generator = card_number_generator(1, 5)
+    assert next(generator) == '0000 0000 0000 0001'
+    assert next(generator) == '0000 0000 0000 0002'
+    assert next(generator) == '0000 0000 0000 0003'
